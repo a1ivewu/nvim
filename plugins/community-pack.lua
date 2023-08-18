891bfaff7fd84be95e34226c37228fb2b3ba4d16
Still Wu <whoeverimf5@gmail.com>
2023-08-18 17:38:41 +0800

    feat: add html-css package

--- plugins/community-pack.lua
+++ plugins/community-pack.lua
@@ -1,10 +1,10 @@
 return {
   { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.vue" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.cmake" },
-  { import = "astrocommunity.pack.json" },
 }
