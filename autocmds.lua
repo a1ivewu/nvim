2dc8da5e5bc2a3bf9118910b256d8db222bdb286
Still Wu <whoeverimf5@gmail.com>
2023-08-18 21:27:05 +0800

    chore: remove redundant function

--- autocmds.lua
+++ autocmds.lua
@@ -1,22 +1,14 @@
 local opt = vim.opt_local
 local autocmd = vim.api.nvim_create_autocmd
+
 local indent_length = 4
-
-local function set_indent(indent)
-  opt.tabstop = indent
-  opt.softtabstop = indent
-  opt.shiftwidth = indent
-end
-
--- filetype that should be changed its indentation
-local should_change_indent = {
-  "go",
-  "java",
-  "python",
-}
 
 -- set indentation
 autocmd("FileType", {
-  pattern = should_change_indent,
-  callback = function() set_indent(indent_length) end,
+  pattern = { "go", "python", "java" },
+  callback = function()
+    opt.tabstop = indent_length
+    opt.softtabstop = indent_length
+    opt.shiftwidth = indent_length
+  end,
 })
