a25ddfbd2e27c11393b5e596d1bb622c318af86f
Still Wu <whoeverimf5@gmail.com>
2023-11-01 15:06:17 +0800

    chore: remove mode status bar

--- lsp/config/rust_analyzer.lua
+++ lsp/config/rust_analyzer.lua
@@ -2,20 +2,6 @@ return {
   settings = {
     ["rust-analyzer"] = {
       lens = { enable = false },
-      semanticHighlighting = {
-        opeartor = {
-          specialization = { enable = true },
-        },
-        punctuation = {
-          enable = true,
-          spearate = {
-            macro = {
-              bang = { enable = true },
-            },
-          },
-          specialization = { enable = true },
-        },
-      },
     },
   },
 }
